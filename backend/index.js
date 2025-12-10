c3c2ed1c5c9c12930835071f2297783d41c5c8d9
621784 <SatyamKumar@maximus.com>
2025-12-10 17:20:21 +0530

    Backend listen

--- backend/index.js
+++ backend/index.js
@@ -46,9 +46,9 @@ app.get("/api/message", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
  
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0.", () => {
 
-  console.log(` Backend running on port ${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 
 });
 
