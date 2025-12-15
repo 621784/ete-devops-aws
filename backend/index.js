f9f98ba51858fafa6396953cf7c60da12831c89f
621784 <SatyamKumar@maximus.com>
2025-12-15 15:46:31 +0530

    Fix

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.get("/api/message", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
  
-app.listen(PORT, "0.0.0.0.", () => {
+app.listen(PORT, "0.0.0.0", () => {
 
   console.log(`Backend running on port ${PORT}`);
 
